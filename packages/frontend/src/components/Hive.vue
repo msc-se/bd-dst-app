e0b34adc3be919096c69dfd0387337dc89764368
Jonas Wahlun Olesen <jwo@intelligentbanker.com>
2020-12-09 22:24:36 +0100

    Refactor: Set up monorepo Feat: Add preliminary hive functionality

---
+++ packages/frontend/src/components/Hive.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hive</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { HiveClient, thrift, auth, connections } from 'hive-driver'
+
+export default defineComponent({
+  name: 'Hive',
+  async setup() {
+    const client = await new HiveClient(thrift.TCLIService, thrift.TCLIService_types).connect(
+      {
+        host: 'localhost',
+        port: 10000
+      },
+      new connections.TcpConnection(),
+      new auth.NoSaslAuthentication()
+    )
+  }
+})
+</script>
+
+<style scoped></style>
--- packages/frontend/src/shims-vue.d.ts
+++ packages/frontend/src/shims-vue.d.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
